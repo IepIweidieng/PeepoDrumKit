aea65b7b19311e1da0be075ef6071abbbc0ca746
Wei-Cheng Yeh (IID) <iid@ccns.ncku.edu.tw>
2025-03-12 00:54:13 +0800

    fix compile-time error by ensuring now-required IMGUI_DEFINE_MATH_OPERATORS definition order

--- src/imgui/3rdparty_extension/ImGuiColorTextEdit/TextEditor.cpp
+++ src/imgui/3rdparty_extension/ImGuiColorTextEdit/TextEditor.cpp
@@ -8,7 +8,10 @@
 
 #include "TextEditor.h"
 
+#ifndef IMGUI_DEFINE_MATH_OPERATORS
 #define IMGUI_DEFINE_MATH_OPERATORS
+#endif
+
 #include "imgui/3rdparty/imgui.h" // for imGui::GetCurrentWindow()
 #include "imgui/3rdparty/imgui_internal.h" // for imGui::GetCurrentWindow()
 
